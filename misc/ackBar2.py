193b3b24eebbeb01ba34a4617c4a924057a35acb
Yifan Zhou <yifzhou@email.arizona.edu>
2016-11-11 14:35:28 -0700

    commit changes

--- misc/ackBar2.py
+++ misc/ackBar2.py
@@ -102,9 +102,13 @@ def ackBar2(
             trap_pop_s = min(trap_pop_s + dE2_s, nTrap_s)
             trap_pop_f = min(trap_pop_f + dE2_f, nTrap_f)
         elif dt < 1200:
-            # next batch
-            trap_pop_s = min(trap_pop_s * np.exp(-(dt-exptime)/tau_trap_s), nTrap_s)
-            trap_pop_f = min(trap_pop_f * np.exp(-(dt-exptime)/tau_trap_f), nTrap_f)
+            if mode == 'staring':
+                # next batch
+                trap_pop_s = min(trap_pop_s * np.exp(-(dt-exptime)/tau_trap_s), nTrap_s)
+                trap_pop_f = min(trap_pop_f * np.exp(-(dt-exptime)/tau_trap_f), nTrap_f)
+            else:
+                trap_pop_s = min(trap_pop_s + dE2_s, nTrap_s)
+                trap_pop_f = min(trap_pop_f + dE2_f, nTrap_f)
         else:
             trap_pop_s = min(trap_pop_s * np.exp(-(dt-exptime)/tau_trap_s) + next(dTrap_s), nTrap_s)
             trap_pop_f = min(trap_pop_f * np.exp(-(dt-exptime)/tau_trap_f) + next(dTrap_f), nTrap_f)
