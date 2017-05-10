f8dfe24fd27b9edec637c0d298e72c61ae167d11
YifZhou <zhouyifan1012@gmail.com>
2017-05-09 18:51:44 -0700

    bad pixel interpolation functions modified as python3 version

--- HST/dqMask.py
+++ HST/dqMask.py
@@ -1,47 +1,45 @@
 #! /usr/bin/env python
-from __future__ import print_function, division
-from astropy.io import fits
+
 import numpy as np
 
-"""flag the bad pixel and create mask for the bad pixels
+u"""flag the bad pixel and create mask for the bad pixels
 """
 
+from __future__ import absolute_import
 flag_dict = {
-    0: 'OK',
-    1: 'decoding error',
-    2: 'data missing',
-    4: 'bad pixel',
-    8: 'non-zero bias',
-    16: 'hot pixel',
-    32: 'unstable response',
-    64: 'warm pixel',
-    128: 'bad reference',
-    256: 'saturation',
-    512: 'bad flat',
-    2048: 'signal in zero read',
-    4096: 'CR by MD',
-    8192: 'cosmic ray',
-    16384: 'ghost'
+    0: u'OK',
+    1: u'decoding error',
+    2: u'data missing',
+    4: u'bad pixel',
+    8: u'non-zero bias',
+    16: u'hot pixel',
+    32: u'unstable response',
+    64: u'warm pixel',
+    128: u'bad reference',
+    256: u'saturation',
+    512: u'bad flat',
+    2048: u'signal in zero read',
+    4096: u'CR by MD',
+    8192: u'cosmic ray',
+    16384: u'ghost'
 }
 
 
-def dqMask(fnList, imageDim=256, flagList=[4, 16, 32, 256]):
-    """identify certain flagged pixels as bad pixels
-    mark the bad pixels as nan
-
+def dqMask(dq, flagList=[4, 16, 32, 256]):
+    u"""identify certain flagged pixels as bad pixels, and create mask
+    Return
+      mask -- bool array, masked pixel marked as True
     Parameters:
       fnList -- file List to get the dq image
       imageDim -- (default 256 subframe) dq image size
       flagList -- flag used to identify bad pixels
     """
-    dqMask = np.zeros((imageDim, imageDim))
-    for i, fn in enumerate(fnList):
-        dq = fits.getdata(fn, 'dq')
-        for flag in flagList:
-            dqMask += dq // flag % 2
-    dqMask[dqMask != 0] = np.nan
-    dqMask[~np.isnan(dqMask)] = 1
+    dqMask = np.zeros_like(dq, dtype=float)
+    for flag in flagList:
+        dqMask += dq // flag % 2
+    dqMask = dqMask.astype(bool)
     return dqMask
 
-if __name__ == '__main__':
+
+if __name__ == u'__main__':
     pass
