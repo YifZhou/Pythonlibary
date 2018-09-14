6f84828ac1b54cae5aa55692da5bc3e7e1a5a530
YifZhou <zhouyifan1012@gmail.com>
2018-09-14 14:18:40 -0700

    add padimg

--- HST/__init__.py
+++ HST/__init__.py
@@ -2,14 +2,15 @@
 """
 HST related programs
 """
-from HSTsinLC import HSTsinLC
-from HSTtransitLC import HSTtransitLC
-from obsTime import obsTime
+# from HSTsinLC import HSTsinLC
+# from HSTtransitLC import HSTtransitLC
+# from obsTime import obsTime
 from telescopeRoll import telescopeRoll
 from dqMask import dqMask
-from scanningData import scanFile, scanData
+# from scanningData import scanFile, scanData
 from scanMask import makeMask
 from WFC3GrismFlat import calFlat
 from badPixelInterp import badPixelInterp
-from inpaint_array import inpaint_array
+# from inpaint_array import inpaint_array
 from G141SkyCube import G141SkyCube
+from padimg import padimg
